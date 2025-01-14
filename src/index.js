dee8f7b6589ccd487d16b344309b153cfb910604
RA18-1121 <rachit0829@gmail.com>
2025-01-14 18:10:41 +0530

    Add button to create Todos

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import "./styles.css";
 import {Todo} from "./todos";
 import {Project} from "./projects";
 import {List} from "./list";
+import plus from "./img/sidebar/plus.svg";
 import hashtag from "./img/sidebar/hashtag.svg";
 import edit from "./img/main/edit.svg";
 import bin from "./img/main/delete.svg";
@@ -11,9 +12,6 @@ import bin from "./img/main/delete.svg";
     const main = document.querySelector(".main");
     const newProjectForm = document.querySelector(".newProjectForm");
     let list = List();
-
-    const projectOne = Project("First");
-    list.addToList(projectOne);
 
     const sidebarLoad =  () => {
         sidebar.textContent = "";
@@ -29,13 +27,6 @@ import bin from "./img/main/delete.svg";
     }
 
     sidebarLoad();
-
-    const todoOne = Todo("first", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis ad modi totam! Ipsum molestiae, illo quis facilis expedita perferendis itaque ducimus impedit! Vel non, quis mollitia excepturi quidem hic sit!", "24/7/25", 4);
-
-    const todoSecond = Todo("second", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis ad modi totam! Ipsum molestiae, illo quis facilis expedita perferendis itaque ducimus impedit! Vel non, quis mollitia excepturi quidem hic sit!", "22/1/11", 3);
-
-    projectOne.addToProject(todoOne);
-    projectOne.addToProject(todoSecond);
 
     const mainLoad = (project) => {
         main.textContent = "";
@@ -90,9 +81,77 @@ import bin from "./img/main/delete.svg";
             todoDiv.append(unitHeader, todoDesc, todoDate, todoPrio, todoCheckDiv);
             main.append(todoDiv);
         }
+
+        const newTodoDiv = document.createElement("div");
+
+        const newTodoButton = document.createElement("img");
+        newTodoButton.setAttribute("id", "newTodoButton");
+        newTodoButton.src = plus;
+        newTodoDiv.classList.add("newTodoDiv");
+        newTodoButton.addEventListener("click", () => {
+            if(!document.querySelector("#newTodoTitle")){
+                const newTodoForm = document.createElement("form");
+
+                const titleFormDiv = document.createElement("div");
+                const titleLabel = document.createElement("label");
+                titleLabel.setAttribute("for", "newTodoTitle");
+                titleLabel.textContent = "Title:";
+                const titleInput = document.createElement("input");
+                titleInput.type = "text";
+                titleInput.setAttribute("id", "newTodoTitle");
+                titleFormDiv.append(titleLabel, titleInput);
+
+                const descriptionFormDiv = document.createElement("div");
+                const descriptionLabel = document.createElement("label");
+                descriptionLabel.setAttribute("for", "newTodoDescription");
+                descriptionLabel.textContent = "Description:";
+                const descriptionInput = document.createElement("textarea");
+                descriptionInput.setAttribute("id", "newTodoDescription");
+                descriptionFormDiv.append(descriptionLabel, descriptionInput);
+
+                const dateFormDiv = document.createElement("div");
+                const dateLabel = document.createElement("label");
+                dateLabel.setAttribute("for", "newTodoDate");
+                dateLabel.textContent = "Due Date:";
+                const dateInput = document.createElement("input");
+                dateInput.type = "text";
+                dateInput.setAttribute("id", "newTodoDate");
+                dateFormDiv.append(dateLabel, dateInput);
+
+                const priorityFormDiv = document.createElement("div");
+                const priorityLabel = document.createElement("label");
+                priorityLabel.setAttribute("for", "newTodoPriority");
+                priorityLabel.textContent = "Priority:";
+                const priorityInput = document.createElement("input");
+                priorityInput.type = "text";
+                priorityInput.setAttribute("id", "newTodoPriority");
+                priorityFormDiv.append(priorityLabel, priorityInput);
+
+                const formButton = document.createElement("button");
+                formButton.textContent = "Create Todo";
+                formButton.addEventListener("click", (event) => {
+                    event.preventDefault();
+                    if(titleInput.value.length >= 3 && dateInput.value.length >= 1 && priorityInput.value.length >= 1)
+                    {
+                        const newTodo = Todo(titleInput.value, descriptionInput.value, dateInput.value, priorityInput.value);
+                        currentProject.addToProject(newTodo);
+                        mainLoad(currentProject);
+                    }
+                })
+
+                newTodoForm.append(titleFormDiv, descriptionFormDiv, dateFormDiv, priorityFormDiv, formButton);
+                main.append(newTodoForm);
+                }
+        })
+
+        const newTodoText = document.createElement("p");
+        newTodoText.textContent = "Add Todo";
+        newTodoDiv.append(newTodoButton, newTodoText);
+        main.append(newTodoDiv);
+
     }
 
-    mainLoad(projectOne);
+    mainLoad(list.getList()[0]);
 
     const projectAddButton = document.querySelector("#projectAddButton");
     projectAddButton.addEventListener("click", () => {
